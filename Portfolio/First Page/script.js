dad2f6930172d4e863265e5cf281753ca730c410
Candy Siyabonga Khumalo <163538990+skhumal@users.noreply.github.com>
2025-05-11 16:25:52 +0200

    Add files via upload

---
+++ Portfolio/First Page/script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  nav.classList.toggle('active');
+}
+
+let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('nav');
+
+menu.onclick = () => {
+  nav.classList.toggle('active');
+} 
+
+ /*  let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('.nav');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  nav.classList.toggle('active');
+} */
